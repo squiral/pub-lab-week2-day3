a77dce2a6e38e127915d441a48b325b4d7785f02
James Budge <thebudgie@gmail.com>
2019-04-10 11:47:35 +0100

    created pub initialize method

---
+++ pub.rb
@@ -0,0 +1,13 @@
+class Pub
+
+def initialize
+  @name = name
+  @till = 0
+  @drinks = []
+end
+
+
+
+
+
+end
---
+++ specs/pub_spec.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('../drink.rb')
+
+class TestPub < MiniTest::Test
+
+def setup
+  @pub = Pub.new("CiderClan")
+end
+
+
+
+
+
+
+
+
+
+end
